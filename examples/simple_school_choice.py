7beb1b9de282143dfe1c7b836474217eacdf4408
Kyosuke Morita <kyosuke1029@icloud.com>
2022-04-23 19:37:49 +0100

    add an unit test for utils

--- examples/simple_school_choice.py
+++ examples/simple_school_choice.py
@@ -1,5 +1,5 @@
 from deferred_acceptance.deferred_acceptance import deferred_acceptance
-from deferred_acceptance.utils.data_construct import create_dataframes
+from deferred_acceptance.utils import create_dataframes
 
 
 def simple_school_choice() -> None:
---
+++ tests/test_utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from pandas._testing import assert_frame_equal
+from deferred_acceptance.utils import create_dataframes
+
+
+def test_create_dataframes():
+    expected_students_df = pd.DataFrame(
+        {
+            "A": [1, 2, 3, 2],
+            "B": [2, 3, 2, 1],
+            "C": [3, 1, 1, 3]
+        },
+        index=["a", "b", "c", "d"]
+    )
+
+    expected_schools_df = pd.DataFrame(
+        {
+            "A": [1, 2, 3, 4],
+            "B": [1, 3, 2, 4],
+            "C": [2, 3, 4, 1]
+        },
+        index=["a", "b", "c", "d"]
+    )
+
+    students_list = ['a', 'b', 'c', 'd']
+    schools_list = ['A', 'B', 'C']
+    students_preferences = {'a': [1, 2, 3], 'b': [2, 3, 1], 'c': [3, 2, 1], 'd': [2, 1, 3]}
+    schools_preferences = {'A': [1, 2, 3, 4], 'B': [1, 3, 2, 4], 'C': [2, 3, 4, 1]}
+
+    students_df, schools_df = create_dataframes(
+        students_list=students_list,
+        students_preferences=students_preferences,
+        schools_list=schools_list,
+        schools_preferences=schools_preferences
+    )
+
+    assert_frame_equal(
+        students_df,
+        expected_students_df
+    )
+
+    assert_frame_equal(
+        schools_df,
+        expected_schools_df
+    )
